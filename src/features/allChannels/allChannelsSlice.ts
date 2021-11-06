b21997ac0b579ba076999c95916f9dcc31c19e98
Ranjit Jana <40226912+ranjitjana027@users.noreply.github.com>
2021-11-07 01:22:42 +0530

    add channel info modal

--- src/features/allChannels/allChannelsSlice.ts
+++ src/features/allChannels/allChannelsSlice.ts
@@ -14,6 +14,9 @@ type Channel={
     channel_name: string,
     created_on: string,
     last_message: null| Message,
+    members: Array<{
+        [key: string]: string
+    }>,
     members_count: number,
     isMember: boolean,
 }
